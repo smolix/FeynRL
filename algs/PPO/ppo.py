6b6ef057a93fabb09a1bd823a192d12555c4cdbb
rasool <r.fakoor@gmail.com>
2025-12-17 11:55:47 -0800

    init

--- algs/PPO/ppo.py
+++ algs/PPO/ppo.py
@@ -88,13 +88,14 @@ class PPO:
 
         return rets, advs
 
-    def compute_loss(self, logits, y, mask):
+    def compute_loss(self, logits, y, loss_mask):
         '''
-         This functions implements \sum_{i=1}^{N} log p(y_i|x_i)
+         This function implements \sum_{i=1}^{N} log p(y_i|x_i)
          y is target label [B, T -1]
          logits is model prediction [B, T -1, vocab_size]
         '''
-        B, T, vocab_size = logits.shape
+        # [B, T -1, vocab_size]
+        _, _, vocab_size = logits.shape
 
         # flatten logits across batch and seq_len before computing loss
         # so logits is [B * (T -1), vocab_size]
@@ -107,30 +108,46 @@ class PPO:
 
         # We need to apply mask to loss to remove any things 
         # which should not be considered in loss (e.g., padding tokens)
-        masked_loss = per_token_loss * mask
+        loss_mask = loss_mask.view(-1).to(dtype=per_token_loss.dtype)  # [B * (T - 1)]
+        masked_per_token_loss = per_token_loss * loss_mask
 
-        # To avoid gardient accumulation error caused by loss.mean(),
+        # To avoid gradient accumulation error caused by loss.mean(),
         # we use sum of loss instead but play with learning rate to account for this.
-        loss = masked_loss.sum()
+        loss = masked_per_token_loss.sum()
+
+        # Loss_accumulated \neq Loss_full_batch when sequence lengths vary.
+        # to address that, we normalize by total sequence length (constant)
+        # which is fixed across gpus, not valid tokens (variable) which is loss_mask.sum().
+        # This solves the gradient accumulation bug.
+        if self.normalize_loss:
+            total_possible_tokens = logits.shape[0]
+            if total_possible_tokens == 0:
+                # This shouldn't happen
+                raise ValueError("Cannot compute loss: total_possible_tokens is 0")
+
+            loss = loss / total_possible_tokens
+
         return loss
 
     def forward(self, batch):
         '''
             This function implements a single forward pass for current batch.
-            It returns logits, y, and mask
-            logits: [B, T -1, vocab_size]
-            y: [B, T -1]
-            mask: [B, T -1]
+            It returns logits, y, and mask.
+            The size of batch['seq_ids']/batch['seq_attn_mask'] is [B, T]
+            The size of batch['loss_mask'] is [B, T-1]
+            The size of return variables logits, y, and mask would be [B, T-1]
         '''
         # batch is a dictionary, so we want to extract things we need from it
-        # input_ids/att_mask/pos_ids are [B, T]
-        input_ids = batch['input_ids'].to(self.device)
-        att_mask  = batch['attention_mask'].to(self.device)
-        pos_ids   = batch['position_ids'].to(self.device)
-        rewards   = batch['rewards'].to(self.device)
-        masks     = batch['masks'].to(self.device)
-        vals      = batch['vals'].to(self.device)
-        last_val  = batch['last_val'].to(self.device)
+        # input_ids and att_mask are [B, T]
+        input_ids = batch['seq_ids']
+        att_mask  = batch['seq_attn_mask']
+        # loss_mask is [B, T -1]
+        loss_mask = batch['loss_mask'].contiguous()
+
+        # if pos_ids is not provided, HF will add that automatically.
+        pos_ids   = batch.get('position_ids', None)
+        if pos_ids is not None:
+            pos_ids = pos_ids.to(self.device)
 
         # feed data to model
         output = self.model_engine(input_ids=input_ids,
@@ -141,73 +158,56 @@ class PPO:
         # [B, T, vocab_size]
         every_token_logits = output.logits
 
-        # label is input_ids shifted by one
-        # so y is [B, T -1]
+        # label would be input_ids shifted by one (input_ids[:, 1:])
+        # so the size is [B, T-1]
         y = input_ids[:, 1:].contiguous()
-        # as a result we need to remove last token from every_token_logits
-        # so logits is [B, T -1, vocab_size]
+        # it is next token prediction, so we remove last token from logits
         logits = every_token_logits[:, :-1, :].contiguous()
 
-        # last input token is EOS, so we ignore loss theal  e. so mask is [B, T -1]
-        mask = batch['loss_mask'][:, :-1].contiguous()
+        return logits, y, loss_mask
 
-        return logits, y, mask
-
-    def eval_step(self, data):
+    def eval_step(self, micro_batch):
         '''
            This function implements a single validation step per rank/gpu.
         '''
         # we need to split data into micro batches
-        micro_batches = data.split(self.micro_batch_size_per_gpu)
-        num_of_micro_batches = len(micro_batches)
-        val_loss = 0
         self.model_engine.eval()
         with torch.no_grad():
-            for batch in micro_batches:
-                ######## 
-                # forward pass per gpu/rank
-                ########
-                logits, y, mask = self.forward(batch)
+            # forward pass per gpu/rank
+            logits, y, loss_mask = self.forward(micro_batch)
 
-                ######## 
-                # compute loss pass
-                ########
-                loss = self.compute_loss(logits=logits, y=y, mask=mask)
+            # compute loss pass
+            loss = self.compute_loss(logits=logits, y=y, loss_mask=loss_mask)
+            val_loss = loss.item()
 
-                val_loss += loss.item()/num_of_micro_batches
+        return {"loss": float(val_loss)}
 
-        return val_loss
-
-    def train_step(self, data):
+    def train_step(self, micro_batch):
         '''
            This function implements a single training step per rank/gpu.
            The batch size for each gpu/rank should be micro_batch_size_per_gpu. 
-           So we need to split data into micro batches.  
+           The DataLoader already yields micro-batches. 
         '''
-        # we need to split data into micro batches
-        micro_batches = data.split(self.micro_batch_size_per_gpu)
-        num_of_micro_batches = len(micro_batches)
-        step_loss = 0
         # make sure model is in training mode
         self.model_engine.train()
-        for batch in micro_batches:
-            ######## 
-            # forward pass per gpu/rank
-            ########
-            logits, y, mask = self.forward(batch)
 
-            ######## 
-            # compute loss pass
-            ########
-            loss = self.compute_loss(logits=logits, y=y, mask=mask)
+        # 1. For DeepSpeed, zero_grad should be called at the start of the training step
+        # especially important with gradient accu.
+        self.model_engine.zero_grad()
 
-            ########    
-            # backward step
-            ########
-            self.optimizer.zero_grad()
-            loss = loss / num_of_micro_batches            
-            loss.backward()
-            step_loss += loss.item()
+        # 2. forward pass per gpu/rank
+        logits, y, loss_mask = self.forward(micro_batch)
 
+        # 3. compute loss pass
+        loss = self.compute_loss(logits=logits, y=y, loss_mask=loss_mask)
+
+        # 4. backward step
+        # deepspeed aggregates gradients and only updates weights when accumulation_steps is reached.
+        self.model_engine.backward(loss)
+
+        # 5. optimizer step
         self.model_engine.step()
-        return step_loss
+
+        # return loss
+        train_loss = loss.item()
+        return {"loss": float(train_loss)}
